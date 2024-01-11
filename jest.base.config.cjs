0e1180b94a720adcf0e8088d07ae8e5c2d398e99
danielecina <icloud.d@gmail.com>
2024-01-11 12:19:41 +0100

    fix: pipeline?

---
+++ jest.base.config.cjs
@@ -0,0 +1,19 @@
+/** 
+ * @type {import('ts-jest').JestConfigWithTsJest} 
+ */
+
+module.exports = {
+  testMatch: [
+    "**/__tests__/**/*.+(ts)",
+    "**/?(*.)+(spec|test).+(ts)"
+  ],
+  rootDir: "../..",
+  roots: [
+    "<rootDir>/packages/microfrontend-sdk"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", {
+      "tsconfig": "<rootDir>/tsconfig.json"
+    }]
+  }
+}
