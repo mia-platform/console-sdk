1eee128ab3ba44e27a5dcc1115e15d56d37c64d1
danielecina <icloud.d@gmail.com>
2024-01-15 18:00:07 +0100

    fix: tests

--- packages/console-sdk-webpack-helpers/src/renderWebpackMicroApp.test.ts
+++ packages/console-sdk-webpack-helpers/src/renderWebpackMicroApp.test.ts
@@ -17,22 +17,12 @@
  */
 
 import { ConsoleSDK, IConsoleProps } from '@mia-platform/console-sdk-microfrontend'
-import { JSDOM } from 'jsdom'
-import { QiankunProps } from 'vite-plugin-qiankun/dist/helper'
 
 import renderWebpackMicroApp, { IViteParams, decorateLifecycleFunction, getSDK } from './renderWebpackMicroApp'
 
-const dom = new JSDOM()
-const { document } = dom.window
-
-const quiankunContainer = document.createElement('div')
-quiankunContainer.id = '__qiankun_microapp_wrapper_for_microfrontend__'
-
-const consoleProps: IConsoleProps<QiankunProps> = {
+const consoleProps: IConsoleProps = {
   name: 'testMicrofrontend',
-  container: quiankunContainer,
   // some console injected props
-  console: {},
   eventListener: jest.fn(),
   featureTogglesProxyContext: {},
   hotkeysContext: {},
