2ce8318b1e0c271414e912b37c684da5fbd53320
danielecina <icloud.d@gmail.com>
2024-01-15 17:51:16 +0100

    reset: decoration

--- packages/console-sdk-webpack-helpers/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { IConsoleProps } from '@mia-platform/console-sdk-microfrontend'
-
-declare module 'vite-plugin-qiankun/dist/helper' {
-  // IConsoleProps are passed from Quiankun across QuiankusProps to microfrontend 
-  interface QiankunProps extends IConsoleProps {}
-}
-  
