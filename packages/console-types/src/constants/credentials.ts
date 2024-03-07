b6535e662aa3f4feb66730b459aad5a49c718b33
Luca Maltagliati <luca.maltagliati@mia-platform.eu>
2024-03-07 11:50:30 +0100

    revert unneeded changes

--- packages/console-types/src/constants/credentials.ts
+++ packages/console-types/src/constants/credentials.ts
@@ -17,10 +17,9 @@
  */
 
 export enum CredentialsTypes {
-  USER_PASS = 'userPass',
-  TOKEN = 'token',
-  M2M = 'm2m',
-  CLIENT_CREDENTIALS = 'client_credentials',
-  CLIENT_CREDENTIALS_CERTIFICATE = 'client_credentials_certificate',
+    USER_PASS = 'userPass',
+    TOKEN = 'token',
+    M2M = 'm2m',
+    CLIENT_CREDENTIALS = 'client_credentials',
+    CLIENT_CREDENTIALS_CERTIFICATE = 'client_credentials_certificate',
 }
-
