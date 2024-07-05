6acbbcb0c1377c3441a77122080d6ef5bc61c65b
Daniele <danielemarostica@gmail.com>
2024-07-05 10:53:30 +0200

    made baseUrl not mandatory (#175)
    Co-authored-by: Daniele Marostica <daniele.marostica@mia-platform.eu>

--- packages/console-client/src/test/client/index.test.ts
+++ packages/console-client/src/test/client/index.test.ts
@@ -26,16 +26,53 @@ import { Extensions } from '../../kiota-client/api/extensibility/tenants/item/ex
 const baseUrl = 'https://base-url.com'
 
 describe('console-client', () => {
-  it('throws if baseUrl is not set', async() => {
+  it('calls APIs correctly without baseUrl set', async(t) => {
+    const testTenantId = 'some-tenant-id'
+    const apiPath = `/api/extensibility/tenants/${testTenantId}/extensions`
+    const expectedUrl = apiPath
+
+    const mockedExtensions: Extensions[] = [
+      {
+        extensionId: 'some-id-1',
+        name: 'extension-1',
+        description: 'this is extension 1',
+      },
+    ]
+
+    const axiosMock = t.mock.method(axiosWrapper, 'axiosFn', async(url: string) => {
+      assert.equal(url, expectedUrl)
+
+      const response = {
+        data: mockedExtensions,
+        status: 200,
+        headers: new Map<string, string>(),
+      }
+      return Promise.resolve(response)
+    })
+
     const consoleClient = new ConsoleClient('')
 
-    const testApi = async(): Promise<Extensions[]> => {
-      const extensions = await consoleClient.extensibility
-        .tenants.byTenantId('some-tenant-id')
-        .extensions.get()
-      return extensions || []
-    }
-    await assert.rejects(() => testApi(), /baseUrl cannot be falsy/i)
+    const extensions = await consoleClient.extensibility
+      .tenants.byTenantId(testTenantId)
+      .extensions.get()
+
+    const { calls } = axiosMock.mock
+    assert.equal(calls.length, 1)
+
+    const [axiosCall] = calls
+    assert.deepEqual(axiosCall.arguments, [
+      expectedUrl,
+      {
+        data: undefined,
+        headers: [
+          ['accept', 'application/json'],
+          ['content-type', 'application/json'],
+          ['user-agent', 'console-client'],
+        ],
+        method: 'GET',
+      },
+    ])
+    assert.deepEqual(extensions, mockedExtensions)
   })
 
   it('sets default headers correctly', async(t) => {
