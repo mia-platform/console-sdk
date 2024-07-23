eccd8e28eec3803230b8bd9c237747da7933b14a
ThisIsDemetrio <Demetrio Marino>
2024-07-23 14:22:07 +0200

    feat: add tests for GET, fix test for PUT

--- packages/console-client/src/test/client/index.test.ts
+++ packages/console-client/src/test/client/index.test.ts
@@ -23,6 +23,7 @@ import { AxiosError, AxiosResponse } from 'axios'
 import axiosWrapper from '../../client/http/axios'
 import { ConsoleClient } from '../../client'
 import { Extensions, ExtensionsPutRequestBody } from '../../kiota-client/api/extensibility/tenants/item/extensions'
+import { WithExtensionGetResponse } from '../../kiota-client/api/extensibility/tenants/item/extensions/item'
 
 const baseUrl = 'https://base-url.com'
 
@@ -86,6 +87,73 @@ describe('console-client', () => {
     const testTenantId = 'some-tenant-id'
     const testExtensionId = 'the-extension-id'
 
+    describe('GET /{id} method', () => {
+      it('properly invokes api', async(t) => {
+        const expectedUrl = `/api/extensibility/tenants/${testTenantId}/extensions/${testExtensionId}`
+        const expectedResult: WithExtensionGetResponse = {
+          extensionId: 'the-extension-id',
+          activationContexts: ['company'],
+          name: 'The Extension',
+          type: 'iframe',
+          entry: 'http://my-extension:8000',
+        }
+
+        const mock = async(url: string): Promise<unknown> => {
+          assert.equal(url, expectedUrl)
+          return Promise.resolve({ data: expectedResult })
+        }
+        const axiosMock = t.mock.method(axiosWrapper, 'axiosFn', mock)
+
+        const response = await new ConsoleClient('').extensibility
+          .tenants.byTenantId(testTenantId)
+          .extensions
+          .byExtensionId(testExtensionId)
+          .get()
+
+        const { calls } = axiosMock.mock
+        assert.equal(calls.length, 1)
+
+        const [axiosCall] = calls
+        assert.deepEqual(axiosCall.arguments, [
+          expectedUrl,
+          {
+            data: undefined,
+            headers: {
+              'accept': 'application/json',
+              'content-type': 'application/json',
+              'user-agent': 'console-client',
+            },
+            method: 'GET',
+          },
+        ])
+
+        assert.deepEqual(response, expectedResult)
+      })
+
+      it('handles error', async(t) => {
+        const expectedUrl = `/api/extensibility/tenants/${testTenantId}/extensions/${testExtensionId}`
+        const expectedError = {
+          statusCode: 500,
+          error: 'some-error',
+          message: 'some-message',
+        }
+
+        const mock = async(url: string): Promise<unknown> => {
+          assert.equal(url, expectedUrl)
+          return Promise.reject(buildAxiosError(expectedError))
+        }
+
+        t.mock.method(axiosWrapper, 'axiosFn', mock)
+
+        const response = new ConsoleClient('').extensibility
+          .tenants.byTenantId(testTenantId).
+          extensions.byExtensionId(testExtensionId)
+          .get()
+
+        await assert.rejects(response, expectedError)
+      })
+    })
+
     describe('PUT method', () => {
       const expectedUrl = `/api/extensibility/tenants/${testTenantId}/extensions`
 
@@ -121,7 +189,7 @@ describe('console-client', () => {
         assert.deepEqual(
           JSON.parse(Buffer.from(axiosCall.arguments[1]?.data as Uint8Array).toString()),
           {
-            contexts: ['company', 'project'],
+            contexts: 'company, project',
             name: 'extension name',
           }
         )
