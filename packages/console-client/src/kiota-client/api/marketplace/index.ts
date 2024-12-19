72ced8a236ae892841a55f0339ca09fa0799e14b
Federico Maggi <7142570+fredmaggiowski@users.noreply.github.com>
2024-12-19 15:00:23 +0100

    Update packages/console-client/src/kiota-client/api/marketplace/index.ts
    Co-authored-by: Daniele <danielemarostica@gmail.com>

--- packages/console-client/src/kiota-client/api/marketplace/index.ts
+++ packages/console-client/src/kiota-client/api/marketplace/index.ts
@@ -63,7 +63,7 @@ export interface MarketplaceRequestBuilderGetQueryParameters {
 /**
  * Uri template for the request builder.
  */
-export const MarketplaceRequestBuilderUriTemplate = "{+baseurl}/api/marketplace";
+export const MarketplaceRequestBuilderUriTemplate = "{+baseurl}/api/marketplace{?includeTenantId*,itemUrns*,perPage*,publishOnMiaDocumentation*,resolveResourcesData*,tenantId*,types*}";
 /**
  * Metadata for all the navigation properties in the request builder.
  */
