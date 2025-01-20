83ef994b6c5f7a56f9df6fb00aca36102594f0dc
epessina <edoardopessina.priv@gmail.com>
2025-01-20 12:50:04 +0100

    Added tests

---
+++ packages/console-types/src/types/catalog/item-manifest.test.ts
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import Ajv from 'ajv'
+import t from 'tap'
+import addFormats from 'ajv-formats'
+
+import { validationMessage } from '../validate-utils.test'
+import { CatalogItemManifest, catalogItemManifestSchema } from './item-manifest'
+import { CatalogDocumentationType, CatalogReleaseStage } from './commons'
+
+t.test('catalog', t => {
+  const ajv = new Ajv()
+  addFormats(ajv)
+  const validate = ajv.compile<CatalogItemManifest>(catalogItemManifestSchema)
+
+  t.test('only required fields', t => {
+    const data: CatalogItemManifest = {
+      type: 'type',
+      itemId: 'item-id',
+      name: 'name',
+      resources: { foo: 'bar' },
+      tenantId: 'tenant-id',
+    }
+
+    t.ok(validate(data), validationMessage(validate.errors))
+
+    t.end()
+  })
+
+  t.test('all fields', t => {
+    const data: CatalogItemManifest = {
+      type: 'type',
+      itemId: 'item-id',
+      name: 'name',
+      resources: { foo: 'bar' },
+      tenantId: 'tenant-id',
+      categoryId: 'category-id',
+      comingSoon: true,
+      description: 'description',
+      documentation: { type: CatalogDocumentationType.EXTERNAL_LINK, url: 'http://example.com' },
+      imageUrl: 'http://example.com',
+      isVersioningSupported: true,
+      providerId: 'provider-id',
+      publishOnMiaDocumentation: true,
+      releaseStage: CatalogReleaseStage.BETA,
+      repositoryUrl: 'http://example.com',
+      supportedBy: 'supported-by',
+      supportedByImageUrl: 'http://example.com',
+      version: { name: '1.0.0', releaseNote: 'release-note', security: true },
+      visibility: { allTenants: true, public: true },
+    }
+
+    t.ok(validate(data), validationMessage(validate.errors))
+
+    t.end()
+  })
+
+  t.end()
+})
