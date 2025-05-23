c40115803dd451434f064a99a2c218188f3db96e
daniele.cina <daniele.cina@mia-platform.eu>
2025-05-23 15:21:21 +0200

    fix(tests): update endpoint types configuration

--- packages/console-types/src/types/config.test.ts
+++ packages/console-types/src/types/config.test.ts
@@ -18,12 +18,14 @@
  */
 
 import Ajv from 'ajv'
-import t from 'tap'
+import * as t from 'tap'
 
 import { Config, config } from './config'
 import ajvConsoleErrors from '../plugins/ajv-console-errors'
 import constants from '../constants'
 import { validationMessage } from './validate-utils.test'
+import { Verbs, EndpointTypes } from '../constants/endpoints'
+import { EnvironmentVariablesTypes } from '../constants/services'
 
 const { ServiceTypes } = constants
 
@@ -124,181 +126,195 @@ t.test('services', t => {
 
   t.test('test', t => {
     const config: Config = {
-      'endpoints': {
+      endpoints: {
         '/secretbooks': {
-          'basePath': '/secretbooks',
-          'routes': {
+          basePath: '/secretbooks',
+          routes: {
             'GET/': {
-              'id': 'GET/',
-              'path': '/',
-              'public': {
-                'inherited': true,
+              id: 'GET/',
+              path: '/',
+              public: {
+                inherited: true,
               },
-              'showInDocumentation': {
-                'inherited': true,
+              showInDocumentation: {
+                inherited: true,
               },
-              'secreted': {
-                'inherited': true,
+              secreted: {
+                inherited: true,
               },
-              'acl': {
-                'inherited': true,
+              acl: {
+                inherited: true,
               },
-              'backofficeAcl': {
-                'inherited': true,
+              backofficeAcl: {
+                inherited: true,
               },
-              'verb': 'GET',
-              'allowUnknownRequestContentType': {
-                'inherited': true,
+              verb: Verbs.METHOD_GET,
+              allowUnknownRequestContentType: {
+                inherited: true,
               },
-              'allowUnknownResponseContentType': {
-                'inherited': true,
+              allowUnknownResponseContentType: {
+                inherited: true,
               },
-              'preDecorators': [],
-              'postDecorators': [],
-              'rateLimit': {
-                'inherited': true,
+              preDecorators: [],
+              postDecorators: [],
+              rateLimit: {
+                inherited: true,
               },
             },
             'POST/': {
-              'id': 'POST/',
-              'path': '/',
-              'public': {
-                'inherited': true,
+              id: 'POST/',
+              path: '/',
+              public: {
+                inherited: true,
               },
-              'showInDocumentation': {
-                'inherited': true,
+              showInDocumentation: {
+                inherited: true,
               },
-              'secreted': {
-                'inherited': true,
+              secreted: {
+                inherited: true,
               },
-              'acl': {
-                'inherited': true,
+              acl: {
+                inherited: true,
               },
-              'backofficeAcl': {
-                'inherited': true,
+              backofficeAcl: {
+                inherited: true,
               },
-              'verb': 'POST',
-              'allowUnknownRequestContentType': {
-                'inherited': true,
+              verb: Verbs.METHOD_POST,
+              allowUnknownRequestContentType: {
+                inherited: true,
               },
-              'allowUnknownResponseContentType': {
-                'inherited': true,
+              allowUnknownResponseContentType: {
+                inherited: true,
               },
-              'preDecorators': [],
-              'postDecorators': [],
-              'rateLimit': {
-                'inherited': true,
+              preDecorators: [],
+              postDecorators: [],
+              rateLimit: {
+                inherited: true,
               },
             },
           },
-          'type': 'crud',
-          'public': true,
-          'showInDocumentation': true,
-          'secreted': true,
-          'acl': 'clientType == "secret_key"',
-          'pathName': '/',
-          'collectionId': 'books',
-          'pathRewrite': '/books',
-          'description': 'Endpoint /secretbooks',
-          'tags': [
+          type: EndpointTypes.CRUD,
+          public: true,
+          showInDocumentation: true,
+          secreted: true,
+          acl: 'clientType == "secret_key"',
+          pathName: '/',
+          collectionId: 'books',
+          pathRewrite: '/books',
+          description: 'Endpoint /secretbooks',
+          tags: [
             'crud',
           ],
-          'backofficeAcl': {
-            'inherited': true,
+          backofficeAcl: {
+            inherited: true,
           },
-          'allowUnknownRequestContentType': false,
-          'allowUnknownResponseContentType': false,
-          'forceMicroserviceGatewayProxy': false,
-          'listeners': {
-            'frontend': true,
+          allowUnknownRequestContentType: false,
+          allowUnknownResponseContentType: false,
+          forceMicroserviceGatewayProxy: false,
+          listeners: {
+            frontend: true,
           },
         },
         '/active-users': {
-          'basePath': '/active-users',
-          'routes': {},
-          'type': 'crud',
-          'public': true,
-          'showInDocumentation': true,
-          'secreted': false,
-          'acl': 'true',
-          'pathName': '/',
-          'collectionId': 'active_users',
-          'pathRewrite': '/active-users',
-          'description': 'Endpoint /active-users',
-          'tags': [
+          basePath: '/active-users',
+          routes: {},
+          type: EndpointTypes.CRUD,
+          public: true,
+          showInDocumentation: true,
+          secreted: false,
+          acl: 'true',
+          pathName: '/',
+          collectionId: 'active_users',
+          pathRewrite: '/active-users',
+          description: 'Endpoint /active-users',
+          tags: [
             'data analytics',
           ],
-          'backofficeAcl': {
-            'inherited': true,
+          backofficeAcl: {
+            inherited: true,
           },
-          'allowUnknownRequestContentType': false,
-          'allowUnknownResponseContentType': false,
-          'forceMicroserviceGatewayProxy': false,
-          'listeners': {
-            'frontend': true,
+          allowUnknownRequestContentType: false,
+          allowUnknownResponseContentType: false,
+          forceMicroserviceGatewayProxy: false,
+          listeners: {
+            frontend: true,
           },
         },
         '/echo': {
-          'basePath': '/echo',
-          'type': 'custom',
-          'public': true,
-          'showInDocumentation': true,
-          'secreted': false,
-          'acl': 'true',
-          'service': 'echo-service',
-          'port': '80',
-          'pathRewrite': '/',
-          'description': '',
-          'tags': [
+          basePath: '/echo',
+          type: EndpointTypes.CUSTOM,
+          public: true,
+          showInDocumentation: true,
+          secreted: false,
+          acl: 'true',
+          service: 'echo-service',
+          port: '80',
+          pathRewrite: '/',
+          description: '',
+          tags: [
             'echo-service',
           ],
-          'backofficeAcl': {
-            'inherited': true,
+          backofficeAcl: {
+            inherited: true,
           },
-          'allowUnknownRequestContentType': false,
-          'allowUnknownResponseContentType': false,
-          'forceMicroserviceGatewayProxy': false,
-          'listeners': {
-            'frontend': true,
+          allowUnknownRequestContentType: false,
+          allowUnknownResponseContentType: false,
+          forceMicroserviceGatewayProxy: false,
+          listeners: {
+            frontend: true,
           },
         },
       },
-      'services': {
+      services: {
         'echo-service': {
-          'type': 'custom',
-          'advanced': false,
-          'name': 'echo-service',
-          'dockerImage': 'docker.io/davidebianchi/echo-service',
-          'replicas': 1,
-          'logParser': 'mia-json',
-          'containerPorts': [
+          type: ServiceTypes.CUSTOM,
+          advanced: false,
+          name: 'echo-service',
+          dockerImage: 'docker.io/davidebianchi/echo-service',
+          replicas: 1,
+          logParser: 'mia-json',
+          containerPorts: [
             {
-              'name': 'http',
-              'from': 80,
-              'to': 3000,
+              name: 'http',
+              from: 80,
+              to: 3000,
             },
           ],
-          'environment': [
+          environment: [
             {
-              'name': 'HTTP_PORT',
-              'valueType': 'plain',
-              'value': '3000',
+              name: 'HTTP_PORT',
+              valueType: EnvironmentVariablesTypes.PLAIN_TEXT,
+              value: '3000',
             },
           ],
-          'tags': [
+          tags: [
             'custom',
           ],
         },
         'swagger-aggregator': {
-          'type': 'custom',
-          'name': 'swagger-aggregator',
-          'dockerImage': 'nexus.mia-platform.eu/core/swagger-aggregator:3.9.1',
-          'replicas': 1,
-          'description': 'Use Mia-Platform core Swagger Aggregator',
+          type: ServiceTypes.CUSTOM,
+          advanced: false,
+          name: 'swagger-aggregator',
+          dockerImage: 'nexus.mia-platform.eu/core/swagger-aggregator:3.9.1',
+          replicas: 1,
+          description: 'Use Mia-Platform core Swagger Aggregator',
         },
       },
-      'version': '0.61.0',
-      'platformVersion': '10.1.0',
+      collections: {},
+      cmsCategories: {},
+      cmsSettings: {
+        accessGroupsExpression: '',
+      },
+      cmsAnalytics: {},
+      decorators: {
+        preDecorators: {},
+        postDecorators: {},
+      },
+      secrets: [],
+      groups: [],
+      cmsDashboard: [],
+      version: '0.61.0',
+      platformVersion: '10.1.0',
     }
     t.ok(validate(config), validationMessage(validate.errors))
     t.end()
