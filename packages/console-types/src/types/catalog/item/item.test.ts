bcb1bc8873c6d2348fcf70402184c31e600a0445
epessina <edoardopessina.priv@gmail.com>
2025-06-30 14:16:20 +0200

    add to schema

--- packages/console-types/src/types/catalog/item/item.test.ts
+++ packages/console-types/src/types/catalog/item/item.test.ts
@@ -34,6 +34,7 @@ t.test('catalog item', t => {
     const data: CatalogItem = {
       releaseDate: new Date().toISOString(),
       type: 'type',
+      itemTypeDefinitionRef: 'urn:mia-platform:sc:itd:type',
       itemId: 'item-id',
       name: 'name',
       tenantId: 'tenant-id',
@@ -49,6 +50,7 @@ t.test('catalog item', t => {
   t.test('all fields', t => {
     const data: Required<CatalogItem> = {
       type: 'type',
+      itemTypeDefinitionRef: 'urn:mia-platform:sc:itd:type',
       itemId: 'item-id',
       name: 'name',
       tenantId: 'tenant-id',
